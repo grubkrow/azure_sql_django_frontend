a52a3118ce92472e55b5ea794c603e9d5bda980b
grubkrow <kongpob921@gmail.com>
2026-02-05 17:13:14 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'kongphob-backend-c6h4g8fjahbwdjea.japaneast-01.azurewebsites.net/api',
+  baseURL: 'https://kongphob-backend-c6h4g8fjahbwdjea.japaneast-01.azurewebsites.net/api',
   // baseURL: 'http://127.0.0.1:8000/api', // Local API
   headers: {
     'Content-Type': 'application/json',
